1236f6ac17fe4dd02270a890087dec3328f6897e
coder-2011 <coder-2011@users.noreply.github.com>
2026-06-25 22:09:39 +0000

    Stop tracking clang CUDA compatibility shim

--- tools/clang_cuda_compat/texture_fetch_functions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#if CUDA_VERSION >= 12000
-// Clang 18 still includes this removed CUDA texture header while setting up
-// CUDA 12 wrappers. This lint-only shim keeps legacy texture references parsable.
-template <class T, int texType, enum cudaTextureReadMode mode>
-struct texture {};
-#endif
